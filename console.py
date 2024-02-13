d959b87a8a9af9dda199cedf608a81241cd98199
Loay Al-Said <l.programming555@gmail.com>
2024-02-13 20:49:11 +0200

    Adapt filestorage and console with the new files

--- console.py
+++ console.py
@@ -4,12 +4,23 @@
 
 import cmd
 from models.base_model import BaseModel
-from models.user import User
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 classes = {"BaseModel": BaseModel,
-           "User": User}
+           "User": User,
+           "City": City,
+           "State": State,
+           "Amenity": Amenity,
+           "Place": Place,
+           "Review": Review,
+           }
 
 
 class HBNBCommand(cmd.Cmd):
